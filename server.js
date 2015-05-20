a0aed3dfce5fe0e803f0f00f5a35a77c77674177
kyrie_leison <yuka.sato@gaiax.com>
2015-05-20 09:34:33 +0900

    静的ファイルサーバの作成

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http'); // HTTPのサーバ/クライアント機能を提供
+var fs = require('fs');     // ファイルシステム関連の機能を提供
+var path = require('path'); // ファイルシステムのパスに関する機能を提供
+var mime = require('mime'); // ファイルの拡張子にもとづいてMIMEタイプを推論する機能を提供
+var cache = {};             // ファイルの内容を格納
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+  if (cache[absPath]) {
+    sendFile(renponse, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function(exists) {
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+  if (request.url == '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + request.url;
+  }
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(process.env.PORT || 5000, function(){
-  console.log('listening on *:5000');
+server.listen(3000, function() {
+  console.log("Server listening on port 3000");
 });
